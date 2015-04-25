ede431bc8aee9c202e75f03abf706343c8f23f89
Paul Doran <dorzey@gmail.com>
2015-04-25 21:52:17 +0100

    fixing names

--- test/clj_github_issues/group_test.clj
+++ test/clj_github_issues/group_test.clj
@@ -1,31 +1,31 @@
 (ns clj-github-issues.group-test
   (:require [clojure.test :refer :all]
-            [clj-github-issues.group :refer :all :as sort]))
+            [clj-github-issues.group :refer :all :as group]))
 
 (deftest get-labels-test
   (testing "getting labels at top level"
     (is (= [:a :b :c]
-           (#'sort/get-labels {:labels [:a :b :c]})))))
+           (#'group/get-labels {:labels [:a :b :c]})))))
 
 (deftest match?-test
   (testing "true when labels match"
-    (is (true? (#'sort/match? "one" "one"))))
+    (is (true? (#'group/match? "one" "one"))))
   (testing "false when labels don't match"
-    (is (false? (#'sort/match? "one" "two")))))
+    (is (false? (#'group/match? "one" "two")))))
 
 (def simple-issue {:labels [{:name "a"}]})
 
 (deftest has?-test
   (testing "true when issue has label"
-    (is (true? (#'sort/has?  simple-issue "a"))))
+    (is (true? (#'group/has?  simple-issue "a"))))
   (testing "false when issue does not have label"
-    (is (nil? (#'sort/has? simple-issue "b")))))
+    (is (nil? (#'group/has? simple-issue "b")))))
 
 (deftest return-if-has-issue
   (testing "issue is return if it has label"
-    (is (= simple-issue (#'sort/return-if-has simple-issue "a"))))
+    (is (= simple-issue (#'group/return-if-has simple-issue "a"))))
   (testing "issue not returned if it does not have label"
-    (is (nil? (#'sort/return-if-has simple-issue "b")))))
+    (is (nil? (#'group/return-if-has simple-issue "b")))))
 
 (def list-of-issues [{:labels [{:name "a"}]}
                      {:labels [{:name "a"}, {:name "b"}]}
@@ -33,7 +33,7 @@
                      {:labels [{:name "d"}]}])
 
 (deftest group-issues-by-test
-  (let [ret (sort/group-issues-by list-of-issues ["a" "b" "c" "d"])]
+  (let [ret (group/group-issues-by list-of-issues ["a" "b" "c" "d"])]
     (testing "the 'a' group has two"
       (is (= 2 (count (get ret "a")))))
     (testing "the 'b' group has two"
